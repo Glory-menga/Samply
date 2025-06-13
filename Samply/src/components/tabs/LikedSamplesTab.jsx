13aba47d577163aebf5720710306782326f0e048
Glory Menga <mengaglory@gmail.com>
2025-06-13 10:28:50 +0200

    comments on tabs done

--- Samply/src/components/tabs/LikedSamplesTab.jsx
+++ Samply/src/components/tabs/LikedSamplesTab.jsx
@@ -2,17 +2,23 @@ import { Link } from "react-router";
 import { Heart } from "lucide-react";
 import '../../css/Tabs.css';
 
+/**
+ * LikedSamplesTab component
+ * - Displays a clickable tab linking to the "Liked Samples" page
+ * - Includes a heart icon and text
+ * - Styled using external CSS
+ */
 function LikedSamplesTab() {
- return (
+  return (
     <>
-        <div className="tab">
-            <Link to='/liked-samples' className="liked-samples">
-                <Heart size={24} strokeWidth={1} color='#000'/>
-                <p className="liked-samples-text">Liked Samples</p>
-            </Link>
-        </div>
+      <div className="tab">
+        <Link to='/liked-samples' className="liked-samples">
+          <Heart size={24} strokeWidth={1} color='#000' />
+          <p className="liked-samples-text">Liked Samples</p>
+        </Link>
+      </div>
     </>
- );
+  );
 }
 
-export default LikedSamplesTab
+export default LikedSamplesTab;
--- Samply/src/components/tabs/SavedSamplesTab.jsx
+++ Samply/src/components/tabs/SavedSamplesTab.jsx
@@ -2,17 +2,23 @@ import { Link } from "react-router";
 import { Save } from "lucide-react";
 import '../../css/Tabs.css';
 
+/**
+ * SavedSamplesTab component
+ * - Displays a clickable tab linking to the "Saved Samples" page
+ * - Includes a save icon and descriptive text
+ * - Styled using external CSS
+ */
 function SavedSamplesTab() {
- return (
+  return (
     <>
-        <div className="tab">
-            <Link to='/saved-samples' className="saved-samples">
-                <Save size={24} strokeWidth={1} color='#000'/>
-                <p className="saved-samples-text">Saved Samples</p>
-            </Link>
-        </div>
+      <div className="tab">
+        <Link to='/saved-samples' className="saved-samples">
+          <Save size={24} strokeWidth={1} color='#000' />
+          <p className="saved-samples-text">Saved Samples</p>
+        </Link>
+      </div>
     </>
- );
+  );
 }
 
-export default SavedSamplesTab
+export default SavedSamplesTab;
